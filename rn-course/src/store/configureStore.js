afecb6fcea175017488e49638cba05e8510bc112
Max Magee <maxmagee@users.noreply.github.com>
2018-04-23 15:52:16 -0500

    Added debugging integration with React Native Debugger

--- rn-course/src/store/configureStore.js
+++ rn-course/src/store/configureStore.js
@@ -1,12 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import placesReducer from './reducers/places';
 
 const rootReducer = combineReducers({
     places: placesReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {  // eslint-disable-line no-undef
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;  // eslint-disable-line no-undef, no-underscore-dangle, max-len
+}
+
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;
