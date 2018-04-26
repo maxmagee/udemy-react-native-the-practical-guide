aaaf484f3744f50bafbda1436ed3d963903f12c5
Max Magee <maxmagee@users.noreply.github.com>
2018-04-25 21:26:51 -0500

    Incorporated Redux to the screens.

--- rn-course/App.js
+++ rn-course/App.js
@@ -1,13 +1,36 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
+// Screen Imports
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
 // Register Screens
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
-Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen);
-Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen);
+Navigation.registerComponent(
+  'awesome-places.AuthScreen', 
+  () => AuthScreen,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  'awesome-places.SharePlaceScreen', 
+  () => SharePlaceScreen,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  'awesome-places.FindPlaceScreen', 
+  () => FindPlaceScreen,
+  store,
+  Provider
+);
 
 // Start an App
 Navigation.startSingleScreenApp({
--- rn-course/src/store/actions/places.js
+++ rn-course/src/store/actions/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 // this one is using ES6 syntax of placeName: placeName  -->  placeName
 export const addPlace = (placeName) => {
@@ -11,18 +11,5 @@ export const addPlace = (placeName) => {
 export const deletePlace = () => {
     return {
         type: DELETE_PLACE
-    };
-};
-
-export const selectPlace = (key) => {
-    return {
-        type: SELECT_PLACE,
-        placeKey: key
-    };
-};
-
-export const deselectPlace = () => {
-    return {
-        type: DESELECT_PLACE
     };
 };
