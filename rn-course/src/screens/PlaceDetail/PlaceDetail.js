7e8a39362c0cf485345d4980dc31406589737404
Max Magee <maxmagee@users.noreply.github.com>
2018-04-25 22:13:07 -0500

    Changed place deletion to pop the page off of the stack.

--- rn-course/src/screens/PlaceDetail/PlaceDetail.js
+++ rn-course/src/screens/PlaceDetail/PlaceDetail.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const PlaceDetailScreen = props => {
-    return (
-        <View style={styles.container}>
-            <View>
-                <Image 
-                    source={props.selectedPlace.image}
-                    style={styles.placeImage} 
-                />
-                <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+import { deletePlace } from '../../store/actions/index';
+
+class PlaceDetailScreen extends Component {
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image 
+                        source={this.props.selectedPlace.image}
+                        style={styles.placeImage} 
+                    />
+                    <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
+                        <View style={styles.deleteButton}>
+                            <Icon size={30} name={'ios-trash'} color='red' />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View>
-                <TouchableOpacity onPress={props.onItemDeleted}>
-                    <View style={styles.deleteButton}>
-                        <Icon size={30} name={'ios-trash'} color='red' />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -42,4 +52,10 @@ const styles = StyleSheet.create({
     }
 });
 
-export default PlaceDetailScreen;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetailScreen);
--- rn-course/src/store/actions/places.js
+++ rn-course/src/store/actions/places.js
@@ -8,8 +8,9 @@ export const addPlace = (placeName) => {
     };
 };
 
-export const deletePlace = () => {
+export const deletePlace = (placeKey) => {
     return {
-        type: DELETE_PLACE
+        type: DELETE_PLACE,
+        placeKey
     };
 };
