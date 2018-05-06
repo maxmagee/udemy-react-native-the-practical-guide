ed23c79d5a0cf1ac494b691a007e6b7be0fe9dfd
Max Magee <maxmagee@users.noreply.github.com>
2018-05-06 11:17:32 -0500

    Added marker placement and transition animation to map in PickLocation.

--- rn-course/src/components/PickLocation/PickLocation.js
+++ rn-course/src/components/PickLocation/PickLocation.js
@@ -10,16 +10,46 @@ class PickLocation extends Component {
             latitudeDelta: 0.0122,
             longitudeDelta: 
                 (Dimensions.get('window').width / Dimensions.get('window').height) * 0.0122
-        }
+        },
+        locationChosen: false
+    }
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            ...this.state.focusedLocation,
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        });
+
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            };
+        });
     }
 
     render() {
+        let marker = null;
+
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+        }
         return (
             <View style={styles.container}>
                 <MapView 
                     initialRegion={this.state.focusedLocation}
                     style={styles.map}
-                />
+                    onPress={this.pickLocationHandler}
+                    ref={ref => { this.map = ref; }}
+                >
+                    {marker}
+                </MapView>
                 <View style={styles.button}>
                     <Button title='Locate Me' onPress={() => alert('locate me')} />
                 </View>
